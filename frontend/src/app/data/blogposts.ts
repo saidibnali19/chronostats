fdf70ff43fbd63f60aa6d7e22d1cfa6946a4f7c6
Said Ali <saidibnali19@gmail.com>
2025-11-15 19:12:44 +0300

    fix TypeScript issue

--- frontend/src/app/data/blogposts.ts
+++ frontend/src/app/data/blogposts.ts
@@ -4,16 +4,13 @@ export const blogposts: BlogPost[] = [
     {
         title: "Why You Should Track Your Time — Even If You’re Not Busy",
         imageUrl: "/images/image1.jpg",
-        slug: "why-you-should-track-your-time",
     },
     {
         title: "From Data to Decisions: How Time Insights Drive Real Change",
         imageUrl: "/images/image2.webp",
-        slug: "from-data-to-decisions",
     },
     {
         title: "Building Better Habits with Small Daily Logs",
         imageUrl: "/images/image3.webp",
-        slug: "building-better-habits-with-small-daily-logs",
     },
 ];
